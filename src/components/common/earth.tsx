ab3e104d7a3e9c0b7b9177dc90c7a1d7cf8d906a
Joseph Somerdin <asleeponpluto@gmail.com>
2022-09-05 08:40:27 -0400

    improved dark

--- src/components/common/earth.tsx
+++ src/components/common/earth.tsx
@@ -5,11 +5,12 @@ const Earth = () => {
   const [isDark, setIsDark] = useState(false);
 
   const toggleDark = () => {
+    document.body.classList.toggle('dark', !isDark);
     setIsDark(!isDark);
   };
 
   return (
-    <div className={`${styles.earth} ${isDark ? styles.dark : ''}`}>
+    <div className={styles.earth}>
       <h1 className='fs-primary-heading fw-bold'>hello, friend...</h1>
       <button onClick={toggleDark} className='fs-primary-heading'>{isDark ? '🪐' : '🌑'}</button>
     </div>
