a281f50bb892aab5f67faa2745fc3471e6908eb4
Joseph Somerdin <asleeponpluto@gmail.com>
2022-09-07 01:22:46 -0400

    added react-router and various config files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Earth from './components/common/earth';
 
 function App() {
   return (
-    <Earth />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Earth />} />
+        <Route path='/:moniker' element={<Earth />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/common/earth.tsx
+++ src/components/common/earth.tsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import styles from './earth.module.css';
 
 const Earth = () => {
   const [isDark, setIsDark] = useState(false);
+  const params = useParams();
 
   const toggleDark = () => {
     document.body.classList.toggle('dark', !isDark);
@@ -11,10 +13,14 @@ const Earth = () => {
 
   return (
     <div className={styles.earth}>
-      <h1 className='fs-primary-heading fw-bold'>hello, friend...</h1>
-      <button onClick={toggleDark} className='fs-primary-heading'>{isDark ? '🪐' : '🌑'}</button>
+      <h1 className='fs-primary-heading fw-bold'>
+        hello, {params.moniker ? params.moniker : 'friend'}...
+      </h1>
+      <button onClick={toggleDark} className='fs-primary-heading'>
+        {isDark ? '🪐' : '🌑'}
+      </button>
     </div>
   );
-}
+};
 
 export default Earth;
